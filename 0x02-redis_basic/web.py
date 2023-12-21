41324138aa44c8a61cdb9622f8076bb81f87da30
root <obiomachinwuko@yahoo.com>
2023-12-21 13:21:59 +0000

    Redis basics

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,7 +7,6 @@ import requests
 from functools import wraps
 from typing import Callable
 
-
 def track_get_page(fn: Callable) -> Callable:
     """ Decorator for get_page
     """
@@ -26,7 +25,6 @@ def track_get_page(fn: Callable) -> Callable:
         client.set(f'{url}', response, 10)
         return response
     return wrapper
-
 
 @track_get_page
 def get_page(url: str) -> str:
